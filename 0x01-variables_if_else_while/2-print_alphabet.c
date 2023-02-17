cb9f2ff79d98c0d024c6e55f4987c5961db96097
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-17 05:30:34 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int lowercase
+clrscr();
+for (lowercase= ‘a’; lowercase<= ‘z’; lowercase++)
+putchar(lowercase);
+putchar (‘\n’);
+getch();
+}
