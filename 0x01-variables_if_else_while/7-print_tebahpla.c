d6752bba984f0565815849522e921c8288f20b44
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-17 10:24:38 -0800

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,3 @@
-root@5b63e2ff94b2:/alx-low_level_programming/0x01-variables_if_else_while# cat 7-print_tebahpla.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -12,7 +11,7 @@ root@5b63e2ff94b2:/alx-low_level_programming/0x01-variables_if_else_while# cat 7
 int main(void)
 {
 int i;
-for (i = 'z'; i >= 'a'; i--)
+for (i = 'Z'; i >= 'A'; i--)
 putchar(i);
 putchar('\n');
 return (0);
