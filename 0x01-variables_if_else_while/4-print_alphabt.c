bb3d84dcbd60936023bb146b285e302909ed34bd
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-17 09:17:12 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,11 +11,11 @@
 int main(void)
 {
 char lowercase, e, q;
-q = 'q';
 e = 'e';
+q = 'q';
 for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
 {
-if (lowercase != 'q' && lowercase != 'e')
+if (lowercase != 'e' && lowercase != 'q')
 putchar(lowercase);
 }
 putchar('\n');
