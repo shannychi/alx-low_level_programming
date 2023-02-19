becc908e8615561b089f20a9be500f6e7ab0051f
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-19 04:56:07 -0800

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,10 +10,10 @@
 
 int main(void)
 {
-int i,j;
-for (i = '0'; i < '9'; i++)
+int i, j;
+for (i = '0'; i < '8'; i++)
 {
-for(j = i + 1; j < '9'; j++)
+for (j = i + 1; j < '9'; j++)
 {
 if (i != j)
 {
