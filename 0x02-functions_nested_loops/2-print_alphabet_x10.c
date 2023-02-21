bcfc802079b8ce2a3930bee3b6248423d6a55a9d
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-21 01:01:51 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i, num;
+num = 0;
+while(num < 10)
+{
+for(i = 'a'; i <= 'z'; i++
+{
+_putchar(i);
+}
+num++;
+_putchar('\n');
+}
+}
