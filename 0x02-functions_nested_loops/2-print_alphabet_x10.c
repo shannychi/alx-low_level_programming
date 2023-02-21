0a49213bef703259609a6e8724e6d9d6e5e18be8
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-21 06:10:03 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet_x10 - a function that print alphalbet_x10
  *
  * Return: Always 0.
  */
-int void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 int i, num;
 num = 0;
-while(num < 10)
+while (num < 10)
 {
-for(i = 'a'; i <= 'z'; i++
+for (i = 'a'; i <= 'z'; i++)
 {
 _putchar(i);
 }
