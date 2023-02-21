19a45f7b3b371d13e1bd06995d35f36d963e4ef0
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-21 11:06:55 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ *@one: single letter input
+*@two: single letter input
+ * Return: Always 0.
+ */
+int add(int one, int two)
+{
+return (one + two);
+}
