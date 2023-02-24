8c4b89dd4bc101667dd48a5d79dcdbe3af3e2da7
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-24 08:50:40 -0800

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_triangle - check the code
+ *@size: single letter input
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+int i, j, k;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (j = 0; j < (size - 1); j++)
+{
+for (i = 0; i < (size - 1) - j; i++)
+{
+_putchar(' ');
+}
+for (k = 0; k <= j; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
