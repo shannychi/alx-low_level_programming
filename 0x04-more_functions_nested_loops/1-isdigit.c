47332c2fc92ba3b5ba78418e5372e61017898b9c
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-24 04:01:19 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - check the code
+ *@c: single letter input
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
