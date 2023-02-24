24b97e321dd29e4428d3d4864f4616b8b6bde28b
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-24 06:14:41 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * void more_numbers - check the code
+ * more_numbers - check the code
  *
  * Return: Always 0.
  */
 void more_numbers(void)
 {
 int i, j;
-        for (j = 0; j < 10; j++)
+for (j = 0; j < 10; j++)
 {
-        for(i = 0; i <= 14; i++)
+for (i = 0; i <= 14; i++)
 {
-        if(i >= 10)
-        {
-        _putchar((i / 10) + '0');
-        }
-        else
-        {
-        _putchar((i % 10) + '0');
-        }
-_putchar('\n');
+if (i >= 10)
+{
+_putchar((i / 10) + '0');
 }
+_putchar((i % 10) + '0');
+}
+_putchar('\n');
 }
 }
