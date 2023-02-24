ae07e9471b3b64cc066bef6e11aaa3b17405d515
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-24 10:04:55 -0800

    last task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+_putchar('%d', n);
+_putchar('\n');
+return (0);
+}
