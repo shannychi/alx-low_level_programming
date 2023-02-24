154a0bf8ae8830f1c5e10c9a0f2fccab30bf0f78
Edeh sharon <Pearllizzy196@gmail.com>
2023-02-24 11:04:55 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,23 +2,20 @@
 
 /**
  * print_number - check the code
- *@n: single input
+ *@n: single input letter
  * Return: Always 0.
  */
 void print_number(int n)
 {
+unsigned int i = n;
 if (n < 0)
 {
-n1 = -n;
+n *= -1;
+i = n;
 _putchar('-');
 }
-else
-{
-n1 = n;
-}
-if (n1 / 10)
-{
-print_number(n1 / 10);
-}
-_putchar((n1 % 10) + '0');
+i /= 10;
+if (i != 0)
+print_number(i);
+_putchar((unsigned int) n % 10 + '0');
 }
