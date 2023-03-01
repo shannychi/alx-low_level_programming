eaec65c98e2acec68dfdc73875403b16299fd217
Edeh sharon <Pearllizzy196@gmail.com>
2023-03-01 09:43:03 -0800

    first push

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * char *_strncpy - a function that concatenates two strings.
+ *@src: input
+*@dest: input
+*@n: input
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; src[i] != '\0'; i++)
+if (i < n)
+dest[i] = src[i];
+while (i < n)
+dest[i++] = '\0';
+return (dest);
+}
