92b749c2b9ce4504d07aaee401fae31f5579d212
Edeh sharon <Pearllizzy196@gmail.com>
2023-03-03 03:07:45 -0800

    fixed betty

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * print_number - a function that prints an integer
+ *@n: input
  * Return: Always 0.
  */
 void print_number(int n)
