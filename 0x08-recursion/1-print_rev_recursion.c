d3ff9a54642824169f19ddb2fc1d2dfd76e8baa7
Edeh sharon <Pearllizzy196@gmail.com>
2023-03-08 01:56:59 -0800

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - check the code
+ *@s: the string
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+_print_rev_recursion(s + 1);
+else
+{
+return;
+_putchar(*s);
+}
+}
