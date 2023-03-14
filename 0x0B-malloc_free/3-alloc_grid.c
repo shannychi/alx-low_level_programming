029314e87e8dc0979106eaf4d6085a8222d84eb3
Edeh sharon <Pearllizzy196@gmail.com>
2023-03-14 09:48:27 -0700

    change

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,39 +1,42 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 /**
- * **alloc_grid - function to allocate a grid
- * @width: int type
- * @height: int type
+ * alloc_grid - nested loop to make grid
+ * @width: width input
+ * @height: height input
  * Return: pointer to 2 dim. array
  */
 int **alloc_grid(int width, int height)
 {
-	int x, y;
-	int **pt;
+	int **iarray;
+	int i, n;
 
 	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	iarray = malloc(sizeof(int) * height);
+
+	if (iarray == NULL)
 	{
+		free(iarray);
 		return (NULL);
 	}
-	pt = malloc(height * sizeof(int));
-	if (pt == NULL)
+
+	for (i = 0; i < height; i++)
 	{
-		free(pt);
-		return (NULL);	
-	}
-	for (x = 0; x < height; x++)
-	{
-		pt[x] = malloc(width * sizeof(int));
-		if (pt[x] == NULL)
+		iarray[i] = malloc(sizeof(int) * width);
+		if (iarray == NULL)
 		{
-			free(pt);
+			free(iarray);
 			return (NULL);
 		}
-		for (y = 0; y < width; y++)
+
+		for (n = 0; n < width; n++)
 		{
-			pt[x][y] = 0;
+			iarray[i][n] = 0;
 		}
 	}
-	return (pt);
+	return (iarray);
 }
+
