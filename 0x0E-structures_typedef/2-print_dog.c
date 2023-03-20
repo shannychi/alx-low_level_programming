02b657ef396b5e2e914da5003cd18057f8bd53b9
Edeh sharon <Pearllizzy196@gmail.com>
2023-03-20 09:39:59 -0700

    fixed betty

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,8 +10,8 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-	printf("Name: %s\n", (d->name) ? d->name: "(nil)");
-	printf("Age: %.6f\n", (d->age) ? d->age: 0);
-	printf("Owner: %s\n", ( d->owner) ? d->owner: "(nil)");
+	printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+	printf("Age: %.6f\n", (d->age) ? d->age : 0);
+	printf("Owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
