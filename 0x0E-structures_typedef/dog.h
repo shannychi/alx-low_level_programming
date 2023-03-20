a284bf0250c4ded7756d177034ccb7481ab1a469
Edeh sharon <Pearllizzy196@gmail.com>
2023-03-20 07:47:07 -0700

    fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,17 +2,16 @@
 #define DOG_H
 
 /**
-*struct dog - structure
-*@name: name
-*@age: age
-*@owner: owner
-*/
-
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
 typedef struct dog
 {
-char *name;
-float *age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
