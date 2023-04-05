ac194cc91c6f2360654ab3fc6ba29e585d4e134b
Edeh sharon <Pearllizzy196@gmail.com>
2023-04-05 07:14:32 -0700

    fixed

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,7 +9,7 @@ int sum_listint(listint_t *head)
 {
 	int sum;
 
-	for (sum = 0; head; sum += head->n; head = head->next)
+	for (sum = 0; head; sum += head->n, head = head->next)
 		;
 	return (sum);
 }
