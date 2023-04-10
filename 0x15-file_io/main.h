cd5c46a0ffe8a7e04011298364cb861528fcd1d7
Edeh Sharon <Pearllizzy196@gmail.com>
2023-04-10 08:50:11 -0700

    header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,6 +1,32 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/**
+ * struct copy_struct - struct for cp
+ * @from: from file
+ * @to: to file
+ * @r: rd store
+ * @w: write store
+ * @from_file: open of from file
+ * @to_file: open of to file
+ * @buffer: buffer to read to
+ */
+typedef struct copy_struct
+{
+	char *from;
+	char *to;
+	ssize_t r;
+	ssize_t w;
+	int from_file;
+	int to_file;
+	char *buffer;
+} copy_struct;
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
